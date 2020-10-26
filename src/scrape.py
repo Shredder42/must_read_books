29ee3035bb6119809c2e768d27d63fab26735b92
shredder <atbinder42@gmail.com>
2020-10-26 16:44:43 -0500

    loop through all main page urls

--- src/scrape.py
+++ src/scrape.py
@@ -10,103 +10,113 @@ import time
 
 # How to handle every link in the GR list?
 url = 'https://www.goodreads.com/list/show/264.Books_That_Everyone_Should_Read_At_Least_Once?page=1'
-r = requests.get(url)
 
-r.status_code # printed below
+for i in range(1,3:)
 
-# Step 2: Save into MongoDB
+    r = requests.get(url)
 
-client = MongoClient()
-db = client.goodreads
+    r.status_code # printed below
 
-pages = db.must_read_books
+    # Step 2: Save into MongoDB
 
-pages.insert_one({'html': r.text})
+    client = MongoClient()
+    db = client.goodreads
 
-# Step 3: Parse text to get data
+    pages = db.must_read_books
 
-soup = BeautifulSoup(r.text, 'html.parser')
+    pages.insert_one({'html': r.text})
 
-# this will give me a big thing of whatever is in the entry for each book
-soup.find_all('tr', "")[0]
-entries = soup.find_all('tr', '')[0]
+    # Step 3: Parse text to get data
 
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-# identify the elements that I want
-entries.find('td', 'number').text # ranking
-entries.find('a', 'bookTitle').text.strip() # title
-entries.find('a', 'bookTitle')['href'] # link to book
-entries.find('a', 'authorName').text.rstrip() # author
-entries.find('span', 'minirating').text.strip() # rating
-entries.find('span', 'smallText uitext').find('a').text # score
-entries.find('span', 'smallText uitext').find_all('a')[1].text # votes
-
-# loop through the entries
-b_title = []
-author = []
-ranking = []
-rating = []
-score = []
-votes = []
-links_pt2 = []
-
-for idx, book in enumerate(soup.find_all('tr', '')):
-    b_title.append(book.find('a', 'bookTitle').text.strip())
-    author.append(book.find('a', 'authorName').text.strip())
-    ranking.append(book.find('td', 'number').text)
-    rating.append(book.find('span', 'minirating').text.strip())
-    score.append(book.find('span', 'smallText uitext').find('a').text)
-    votes.append(book.find('span', 'smallText uitext').find_all('a')[1].text)
-    links_pt2.append(book.find('a', 'bookTitle')['href'])
+    # this will give me a big thing of whatever is in the entry for each book
+    soup.find_all('tr', "")[0]
+    entries = soup.find_all('tr', '')[0]
 
 
-# add full url links to go into each page
+    # identify the elements that I want
+    entries.find('td', 'number').text # ranking
+    entries.find('a', 'bookTitle').text.strip() # title
+    entries.find('a', 'bookTitle')['href'] # link to book
+    entries.find('a', 'authorName').text.rstrip() # author
+    entries.find('span', 'minirating').text.strip() # rating
+    entries.find('span', 'smallText uitext').find('a').text # score
+    entries.find('span', 'smallText uitext').find_all('a')[1].text # votes
 
-def combine_url_parts(lst):
+    # loop through the entries
+    b_title = []
+    author = []
+    ranking = []
+    rating = []
+    score = []
+    votes = []
+    links_pt2 = []
 
-    full_links = []
-    for idx, link in enumerate(lst):
-        link_lst = ['https://www.goodreads.com', lst[idx]]
-        full_link = ''.join(link_lst)
-        full_links.append(full_link)
-    return full_links
-
-# Step 4: Go through pulled hyperlinks for more data
-
-b_format = []
-pg_cnt = []
-release = []
-new_release = []
+    for idx, book in enumerate(soup.find_all('tr', '')):
+        b_title.append(book.find('a', 'bookTitle').text.strip())
+        author.append(book.find('a', 'authorName').text.strip())
+        ranking.append(book.find('td', 'number').text)
+        rating.append(book.find('span', 'minirating').text.strip())
+        score.append(book.find('span', 'smallText uitext').find('a').text)
+        votes.append(book.find('span', 'smallText uitext').find_all('a')[1].text)
+        links_pt2.append(book.find('a', 'bookTitle')['href'])
 
 
-for link in full_links:
+    # add full url links to go into each page
 
-    sub_page = requests.get(link)
-    page1.insert_one({'link': link, 'html': sub_page.text})
-    sub_soup = BeautifulSoup(sub_page.text, 'html.parser')
+    def combine_url_parts(lst):
 
-    sub_entries = sub_soup.find_all('div', 'row')
+        full_links = []
+        for idx, link in enumerate(lst):
+            link_lst = ['https://www.goodreads.com', lst[idx]]
+            full_link = ''.join(link_lst)
+            full_links.append(full_link)
+        return full_links
 
-    try:
-        fmt = sub_entries[0].find('span', {'itemprop': 'bookFormat'}).text # book format
-    except:
-        fmt = 'NA'
+    # Step 4: Go through pulled hyperlinks for more data
 
-    pgs = sub_entries[0].find('span', {'itemprop':'numberOfPages'}).text # pages
+    b_format = []
+    pg_cnt = []
+    release = []
+    new_release = []
 
-    try:
-        rls = sub_entries[1].find('nobr', 'greyText').text.strip() # original publication
-    except:
-        rls = 'NA'
 
-    n_rls = sub_entries[1].text.strip()
+    for link in full_links:
 
-    b_format.append(fmt)
-    pg_cnt.append(pgs)
-    release.append(rls)
-    new_release.append(n_rls)
+        sub_page = requests.get(link)
+        page1.insert_one({'link': link, 'html': sub_page.text})
+        sub_soup = BeautifulSoup(sub_page.text, 'html.parser')
 
-    time.sleep(1)
+        sub_entries = sub_soup.find_all('div', 'row')
+
+        try:
+            fmt = sub_entries[0].find('span', {'itemprop': 'bookFormat'}).text # book format
+        except:
+            fmt = 'NA'
+
+        pgs = sub_entries[0].find('span', {'itemprop':'numberOfPages'}).text # pages
+
+        try:
+            rls = sub_entries[1].find('nobr', 'greyText').text.strip() # original publication
+        except:
+            rls = 'NA'
+
+        n_rls = sub_entries[1].text.strip()
+
+        b_format.append(fmt)
+        pg_cnt.append(pgs)
+        release.append(rls)
+        new_release.append(n_rls)
+
+        time.sleep(1)
+
+    # update url
+    new_url = url.split('=')
+    add_1 = int(new_url[1]) + 1
+    new_url[1] = str(add_1)
+    '='.join(new_url)
+    url = new_url
 
 
 # Step 5: Convert to Pandas data frame and .csv file
